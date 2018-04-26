d0bfa68dda764726c3c0b57b77c07f847faa52d7
Matt Crl <matth.carlevaris@gmail.com>
2018-04-26 15:50:04 +0200

    fixed bug

--- src/AppBundle/Entity/Reservation.php
+++ src/AppBundle/Entity/Reservation.php
@@ -12,6 +12,11 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Reservation
 {
+    public function __toString()
+    {
+        return $this->id . ' ';
+    }
+
     /**
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User", inversedBy="passengers")
      * @ORM\JoinColumn(nullable=false)
