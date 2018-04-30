74a57794850ff70bb57271c8d8a75430d6021a48
Matt Crl <matth.carlevaris@gmail.com>
2018-04-30 14:51:01 +0200

    review new routing OK

--- src/AppBundle/Controller/ReviewController.php
+++ src/AppBundle/Controller/ReviewController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
 
 
 
@@ -32,5 +33,18 @@ class ReviewController extends Controller
         return $this->render('review/index.html.twig', ['reviews' => $reviews]
         );
 
+    }
+
+
+    /**
+     * Add a new review.
+     *
+     * @Route("/new", name="review_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction()
+    {
+        // J'ai ajouté la méthode POST pour le futur formulaire de la prochaine quête
+        return $this->render('review/new.html.twig');
     }
 }
